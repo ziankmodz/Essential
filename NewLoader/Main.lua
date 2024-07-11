5e76300b2d0fd2d99778c6cb3652eece826b2b0a
iq-boost <109669438+ziankmodz@users.noreply.github.com>
2024-07-11 12:01:05 +0800

    Create Main.lua

---
+++ NewLoader/Main.lua
@@ -0,0 +1,5 @@
+	local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/violin-suzutsuki/LinoriaLib/main/Library.lua"))()
+
+	Library:Notify("Loading Essential..")
+	
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/ziankmodz/Essential/main/Loader.lua"))()
